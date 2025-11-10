15099a3d38d34d61fdefeb0d7b9b4a8dd07e035d
grethy <gyorgy.rethy@oracle.com>
2025-11-10 09:50:19 +0100

    Fix header

--- declarations/mle-js-plsqltypes/mle-js-plsqltypes.d.ts
+++ declarations/mle-js-plsqltypes/mle-js-plsqltypes.d.ts
@@ -1,5 +1,6 @@
 /**
-/**
+
+Copyright (c) 2019, 2025, Oracle and/or its affiliates.
 
 The Universal Permissive License (UPL), Version 1.0
 
@@ -35,6 +36,8 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
+
+/**
  * Class which implements infix operators for Oracle Number
  * arithmetics: +, -, /, *, etc.
  *
